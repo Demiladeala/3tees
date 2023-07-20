28470166273b758f399cc8e203ee7673095bee50
Oluwademilade Ala <demiladeala@gmail.com>
2023-07-20 20:36:20 +0000

    update

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -16,14 +16,41 @@ const Courses = () => {
     </div>
 
     <div className='w-[96%] mx-auto grid grid-cols-3 grid-rows-2 gap-1 mt-10'>
-     <div className='text-white bg-[#181822] rounded-lg'>
-      <h1>Courses</h1>
-      <p>Lorem ipsum dolor sit amet consectetur.</p>
+     <div className='text-white bg-[#181822] rounded-lg p-2 space-y-2 flex items-center'>
+      <div className=''>
+      <h1 className='pt-6 pb-2 font-semibold text-lg'>Course 1</h1>
+      <p className='text-xs font-light mb-2'>Lorem ipsum dolor sit amet consectetur.</p>
+      <button className='bg-[#E56000] py-1 px-5 rounded-xl mb-8 text-xs'>Button</button>
+      </div>
+     
      </div>
-     <div className='row-span-2 bg-[#F9F9F9] rounded-lg'>2</div>
-     <div className='bg-[#F89650] rounded-lg'>3</div>
-     <div className='bg-[#F89650] rounded-lg'>4</div>
-     <div className='text-white bg-[#181822] rounded-lg'>5</div>
+
+     <div className='row-span-2 bg-[#F9F9F9] rounded-lg p-2 flex items-center'>
+      <div className=''>
+      <h1 className='font-semibold text-lg'>Course 1</h1>
+      <p className='text-xs font-light mb-2'>Lorem ipsum dolor sit amet consectetur.</p>
+      <button className='bg-[#1E1E1E] py-1 px-5 rounded-xl text-white'>Button</button>
+      </div>
+     </div>
+
+     <div className='bg-[#F89650] rounded-lg p-2'>
+      <h1 className='pt-6 font-semibold text-lg'>Course 1</h1>
+      <p className='text-xs font-light mb-2'>Lorem ipsum dolor sit amet consectetur.</p>
+      <button className='bg-[#1E1E1E] py-1 px-5 rounded-xl text-white'>Button</button>
+     </div>
+
+     <div className='bg-[#F89650] rounded-lg p-2'>
+      <h1 className='pt-6 font-semibold text-lg'>Course 1</h1>
+      <p className='text-xs font-light mb-2'>Lorem ipsum dolor sit amet consectetur.</p>
+      <button className='bg-[#1E1E1E] py-1 px-5 rounded-xl text-white'>Button</button>
+     </div>
+
+     <div className='text-white bg-[#181822] rounded-lg p-2'>
+      <h1 className='pt-6 font-semibold text-lg'>Course 1</h1>
+      <p className='text-xs font-light mb-2'>Lorem ipsum dolor sit amet consectetur.</p>
+      <button className='bg-[#E56000] py-1 px-5 rounded-xl'>Button</button>
+     </div>
+
     </div>
     </>
   )
