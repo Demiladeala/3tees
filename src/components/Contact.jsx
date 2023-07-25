6523e307216fae3a68721f0d5a2e651180e557e4
Oluwademilade Ala <demiladeala@gmail.com>
2023-07-25 12:10:39 +0000

    style update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import header from "../assets/contact-header.png"
+import bar1 from '../assets/contact-bar-1.png'
+import bar2 from '../assets/contact-bar-2.png'
+import bar3 from '../assets/contact-bar-3.png'
+import bar4 from '../assets/contact-bar-4.png'
+import bar5 from '../assets/contact-bar-5.png'
+import bar6 from '../assets/contact-bar-6.png'
+import phoneIcon from "../assets/phone-icon.png"
+import WhatsappIcon from "../assets/Whatsapp-icon.png"
+import LocationIcon from "../assets/Location-icon.png"
 
 const Contact = () => {
   return (
     <>
-    <div>
-       CONTACT
+    <div className='w-full relative mt-28'>
+       <div className='relative flex flex-col'>
+        <div className='w-[90%] md:w-[70%] mx-auto bg-[#08043C] rounded-2xl py-8 flex justify-between'>
+          <div className=' border-white basis-[60%] ml-4 space-y-3 text-white'>
+            <div>
+              <img src={header}/>
+            </div>
+            <h1 className='text-lg md:text-2xl'>Find us Through <br className='md:hidden' /> These!</h1>
+          </div>
+          <div className='basis-[40%] flex gap-1 md:justify-center mr-4'>
+            <div className=" h-full">
+              <img className=' w-10' src={bar1} />
+            </div>
+            <div className=" h-full flex flex-col items-center justify-center">
+              <img className=' object-contain w-5' src={phoneIcon}/>
+              <img className=' w-14' src={bar2}/>
+              <img className=' object-contain w-5' src={WhatsappIcon}/>
+            </div>
+            <div className=" h-full flex flex-col items-center justify-center">
+              <img className=' object-contain w-14' src={bar3}/>
+              <img className=' object-contain' src={bar4}/>
+            </div>
+            <div className=" h-full flex flex-col items-center justify-center">
+              <img className=' object-contain mt-4' src={bar5} />
+            </div>
+            <div className=" h-full flex flex-col items-center">
+              <img className=' object-contain' src={bar6} />
+              <img className=' object-contain mt-4' src={LocationIcon} />
+            </div>
+          </div>
+        </div>
+
+        <div className='relative w-[50%] md:w-[30%] bg-[#f1f1f1] rounded-2xl py-2 top-[-1rem] left-[9%] md:left-[20%]'>
+
+        </div>
+       </div>
     </div>
     </>
   )
--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import headerImage from "../assets/header-image.png"
 import rec1 from "../assets/staff-star-1.png"
+import rec2 from "../assets/staff-star-3.png"
 
 const Staff = () => {
   return (
@@ -12,18 +13,47 @@ const Staff = () => {
       <div className='w-full flex items-center justify-center'>
         <img src={headerImage} />
       </div>
-      <h2 className='text-center mt-3 text-2xl font-bold sm:text-2xl'>Everything Starts With The Best Teachers</h2>
+      <h2 className='w-[90%] mx-auto text-center mt-3 text-2xl font-bold sm:text-2xl'>Everything Starts With The Best Teachers</h2>
     </div>
 
-    <div className='w-full mt-20 flex flex-col md:flex-row md:bg-purple-50 md:w-[80%] md:mx-auto '>
+    <div className='w-full mt-20 flex flex-col md:flex-row md:w-[85%] md:mx-auto '>
       <div className='w-[90%] mx-auto flex gap-6'>
-        <div className='bg-[#D9D9D9] rounded-md py-20 basis-[35%] md:basis-[50%]'></div>
-        <div className='bg-[#D9D9D9] rounded-md py-20 basis-[35%]'></div>
+
+        <div className='relative basis-[50%] text-center'>
+          <div className='bg-[#D9D9D9] rounded-md py-20 md:py-32'></div>
+          <h2 className="mt-2 font-semibold text-lg">Position Tag</h2>
+          <p>Name Tag</p>
+        </div>
+
+        <div className='relative basis-[50%] text-center'>
+          <div className='bg-[#D9D9D9] rounded-md py-20 md:py-32'></div>
+          <h2 className="mt-2 font-semibold text-lg">Position Tag</h2>
+          <p>Name Tag</p>
+        </div>
+        
       </div>
-      <div className='w-[90%] mx-auto flex justify-end gap-6 mt-20 md:mt-0'>
-        <div className='bg-[#D9D9D9] rounded-md py-20 basis-[35%]'></div>
-        <div className='bg-[#D9D9D9] rounded-md py-20 basis-[35%]'></div>
+      <div className='w-[90%] mx-auto flex justify-end gap-6 mt-20 md:mt-0 md:ml-6'>
+
+        <div className='relative basis-[50%] text-center'>
+          <div className='bg-[#D9D9D9] rounded-md py-20 md:py-32'></div>
+          <h2 className="mt-2 font-semibold text-lg">Position Tag</h2>
+          <p>Name Tag</p>
+        </div>
+
+        <div className='relative basis-[50%] text-center z-10'>
+          <div className='bg-[#D9D9D9] rounded-md py-20 md:py-32'></div>
+          <h2 className="mt-2 font-semibold text-lg">Position Tag</h2>
+          <p>Name Tag</p>
+        </div>
+
+        <div className='absolute w-full py-32 right-0'>
+          <img className='absolute bottom-0 right-0' src={rec2}/>
+        </div>
       </div>
+    </div>
+
+    <div className='w-full mt-8 flex justify-center items-center'>
+      <button className='border border-black rounded-xl py-2 px-6'>Button</button>
     </div>
     </>
   )
