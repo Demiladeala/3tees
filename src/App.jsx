de3dd62a182032800aa7e4bc7934dee78308df60
Oluwademilade Ala <demiladeala@gmail.com>
2023-07-26 17:54:52 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           <div className="py-8 flex w-[90%] mx-auto items-center justify-end">
             <VscClose size={28} onClick={toggleNavbar} className="cursor-pointer"/>
           </div>
-          <div className="space-y-8 pl-7">
+          <div className="space-y-8 pl-7 flex flex-col text-lg">
             <a href="/#">HOME</a>
             <a href="/#about">ABOUT</a>
             <a href="/#courses">COURSES</a>
